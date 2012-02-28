5e4a1fc26517ef83ddd1f91a89bef522f058968d
andyknowss <andyknowss@gmail.com>
2012-02-28 07:37:28 +0000

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+
+// GET Home Page
+exports.home = function(req, res){
+  res.render('home.html', { title: 'Craigslist Home' })
+};
+
+// GET Search Page
+exports.search = function(req, res){
+  res.render('search.html', { title: 'Craigslist Search' })
+};
+
+// GET Product Page
+exports.product = function(req, res){
+  res.render('product.html', { title: 'Craigslist Product' })
+};
+
+// GET Location Page
+exports.location = function(req, res){
+  res.render('location.html', { title: 'Craigslist Locations' })
+};
