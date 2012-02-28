a9e5151500e0161951fb9ff38c15a24001ef863e
andyknowss@gmail.com <andyknowss@gmail.com@6cf600a4-d1ef-409c-2eaf-d761f8b3b066>
2012-02-28 07:37:28 +0000

    First commit

---
+++ trunk/src/app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.set("view options", {layout: false});
+  app.register('.html', require('ejs'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.home);
+app.get('/home', routes.home);
+app.get('/search', routes.search);
+app.get('/product', routes.product);
+app.get('/location', routes.location);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
